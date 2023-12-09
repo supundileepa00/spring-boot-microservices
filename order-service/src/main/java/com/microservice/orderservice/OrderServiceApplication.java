77f70e0bcf3e2689160c9d404bcbb202a91d62b3
supundileepa00 <paranagamasupun@gmail.com>
2023-12-09 22:11:36 +0530

    Implement product service

--- order-service/src/main/java/com/microservice/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservice/orderservice/OrderServiceApplication.java
@@ -3,7 +3,7 @@ package com.microservice.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
